dfe138d4775ebb654748d5b0309841222462f08e
gyoo <vincent_godbout@cgocable.ca>
2025-03-24 15:34:36 -0400

    Ajouté logos de médias sociaux, changement entre thème sombre et clair et ajustement automatique au ratio de l'écran

---
+++ src/javascript/changement_theme.js
@@ -0,0 +1,29 @@
+var theme_couleur = "clair";
+
+function changer_theme(){
+    var themes = document.getElementsByTagName("link");
+    for (var i = 0; i < themes.length; i++){
+        var href = themes[i].href;
+        if (theme_couleur == "clair"){
+            themes[i].setAttribute("href", href.replace("clair","sombre"));
+        }else{
+            themes[i].setAttribute("href", href.replace("sombre","clair"));
+        }
+    }
+
+    themes = document.getElementsByTagName("img");
+    for (var i = 0; i < themes.length; i++){
+        var href = themes[i].src;
+        if (theme_couleur == "clair"){
+            themes[i].setAttribute("src", href.replace("clair","sombre"));
+        }else{
+            themes[i].setAttribute("src", href.replace("sombre","clair"));
+        }
+    }
+
+    if(theme_couleur == "clair"){
+        theme_couleur = "sombre";
+    }else{
+        theme_couleur = "clair";
+    }
+}
